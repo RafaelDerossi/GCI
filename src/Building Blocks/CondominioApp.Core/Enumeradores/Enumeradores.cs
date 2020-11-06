db81233e07998e8831d4ba86963fa3b559934587
Alexandre Nascimento <alexandre@techdog.com.br>
2020-11-06 10:47:05 -0300

    Classe de usuários e Mobiles

--- src/Building Blocks/CondominioApp.Core/Enumeradores/Enumeradores.cs
+++ src/Building Blocks/CondominioApp.Core/Enumeradores/Enumeradores.cs
@@ -6,4 +6,19 @@
         MASCULINO = 1,
         FEMININO = 2
     }
+
+    public enum TipoDeUsuario
+    {
+        ADMINISTRADORA = 0,
+        ADM = 1,
+        FUNCIONARIO = 2,
+        CLIENTE = 3,
+        SUPERADMIN = 4,
+    }
+
+    public enum Permissao
+    {
+        USUARIO = 0,
+        ADMIN = 1
+    }
 }
---
+++ src/Services/Usuarios/CondominioApp.Usuarios.App/Models/Mobile.cs
@@ -0,0 +1,32 @@
+﻿using CondominioApp.Core.DomainObjects;
+
+namespace CondominioApp.Usuarios.App.Models
+{
+    public class Mobile : Entity
+    {
+        public string DeviceKey { get; private set; }
+
+        public string MobileId { get; private set; }
+
+        public string Modelo { get; private set; }
+
+        public string Plataforma { get; private set; }
+
+        public string Versao { get; private set; }
+
+        //EF
+        public Usuario Usuario { get; set; }
+
+        protected Mobile() { }
+
+        public Mobile(string deviceKey, string mobileId, string modelo, string plataforma, string versao, Usuario usuario)
+        {
+            DeviceKey = deviceKey;
+            MobileId = mobileId;
+            Modelo = modelo;
+            Plataforma = plataforma;
+            Versao = versao;
+            Usuario = usuario;
+        }
+    }
+}
