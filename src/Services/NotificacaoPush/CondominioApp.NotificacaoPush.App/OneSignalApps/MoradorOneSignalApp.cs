c280a739e61699489481f1ba5798318acd8d3b4d
Rafael Derossi <rafael@condominioapp.com>
2021-02-17 14:30:49 -0300

    Ajustes no endpoint de Criar Notificacao

--- src/Services/NotificacaoPush/CondominioApp.NotificacaoPush.App/OneSignalApps/MoradorOneSignalApp.cs
+++ src/Services/NotificacaoPush/CondominioApp.NotificacaoPush.App/OneSignalApps/MoradorOneSignalApp.cs
@@ -2,13 +2,13 @@
 
 namespace CondominioApp.NotificacaoPush.App.OneSignalApps
 {
-    public class CondominioAppV2OneSignalApp : IOneSignalApp
+    public class MoradorOneSignalApp : IOneSignalApp
     {
         public Guid AppId { get; private set; }
 
         public string ApiKey { get; private set; }
 
-        public CondominioAppV2OneSignalApp()
+        public MoradorOneSignalApp()
         {
             AppId = Guid.Parse("abb91fa1-6ed1-4bd8-a347-ed62d1c789b9");
             ApiKey = "ZmQ2ZjI1Y2ItZmEzMy00Y2YyLTkzMzItNmFhNjk2MGRhNDM3";
