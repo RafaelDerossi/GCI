c62dc94a3a6f8021c86959c226383f4546ee93e8
Rafael Derossi <rafael@condominioapp.com>
2021-02-25 18:06:41 -0300

    Fazendo testes de cadastro de disposivitos -WIP

---
+++ Tests/Usuarios/CondominioApp.Usuarios.App.Tests/Mobile/MobileCommandFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CondominioApp.Usuarios.App.Aplication.Commands;
+
+namespace CondominioApp.Usuarios.App.Tests
+{
+    public class MobileCommandFactory
+    {
+        public static CadastrarMobileCommand CadastrarMobileCommandFactory()
+        {
+            return new CadastrarMobileCommand
+                ("ff94bc73-114f-4470-a0bb-e2864815bbdc", "0b2eb4f89990b6e3", "SM-A307GT (10)", "Android", "10", Guid.NewGuid());
+        }
+
+        public static CadastrarMobileCommand CriarComandoCadastroDeMobile()
+        {
+            return CadastrarMobileCommandFactory();
+        }
+
+        public static CadastrarMobileCommand CriarComandoCadastroDeMobile_SemDeviceKey()
+        {
+            var comando = CadastrarMobileCommandFactory();
+            comando.SetDeviceKey("");
+
+            return comando;
+        }
+
+       
+    }
+}
