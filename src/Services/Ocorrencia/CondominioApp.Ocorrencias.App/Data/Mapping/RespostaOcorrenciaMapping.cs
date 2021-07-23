2fb28637cd6be75a8d1dcb0a3162e8d351c16966
Rafael Derossi <rafael@condominioapp.com>
2021-07-23 10:08:21 -0300

    Atualiza BD

--- src/Services/Ocorrencia/CondominioApp.Ocorrencias.App/Data/Mapping/RespostaOcorrenciaMapping.cs
+++ src/Services/Ocorrencia/CondominioApp.Ocorrencias.App/Data/Mapping/RespostaOcorrenciaMapping.cs
@@ -38,7 +38,26 @@ namespace CondominioApp.Ocorrencias.App.Data.Mapping
                     .HasColumnType($"varchar({Foto.NomeFotoMaximo})");
             });
 
+            builder.OwnsOne(u => u.ArquivoAxexo, ft =>
+            {
+                ft.Property(u => u.NomeDoArquivo)
+                    .IsRequired()
+                    .HasMaxLength(NomeArquivo.NomeArquivoMaximo)
+                    .HasColumnName("NomeDoArquivo")
+                    .HasColumnType($"varchar({NomeArquivo.NomeArquivoMaximo})");
 
+                ft.Property(u => u.NomeOriginal)
+                    .IsRequired()
+                    .HasMaxLength(NomeArquivo.NomeArquivoMaximo)
+                    .HasColumnName("NomeOriginal")
+                    .HasColumnType($"varchar({NomeArquivo.NomeArquivoMaximo})");
+
+                ft.Property(u => u.ExtensaoDoArquivo)
+                    .IsRequired()
+                    .HasMaxLength(NomeArquivo.NomeArquivoMaximo)
+                    .HasColumnName("ExtensaoDoArquivo")
+                    .HasColumnType($"varchar({NomeArquivo.NomeArquivoMaximo})");
+            });
         }
     }
 }
