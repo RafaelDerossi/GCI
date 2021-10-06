3589e4467d46ccac354994502d3bed102c661a0f
Rafael Derossi <rafael_derossi@hotmail.com>
2021-10-06 13:34:26 -0300

    API de Pedidos

---
+++ src/Services/Pedidos/NinjaStore.Pedidos.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,34 @@
+﻿using NinjaStore.Core.Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using FluentValidation.Results;
+using NinjaStore.Pedidos.Aplication.Commands;
+using NinjaStore.Pedidos.Domain.Interfaces;
+using NinjaStore.Pedidos.Infra.Data.Repository;
+using NinjaStore.Pedidos.Aplication.Events;
+using NinjaStore.Pedidos.Aplication.Query;
+
+namespace NinjaStore.Pedidos.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {            
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+            
+            //Pedido            
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+            services.AddScoped<INotificationHandler<PedidoAdicionadoEvent>, PedidoEventHandler>();
+
+            //Query            
+            services.AddScoped<IPedidoQuery, PedidoQuery>();
+            
+            //Repositório            
+            services.AddScoped<IPedidoRepository, PedidoRepository>();            
+
+            //Repositório Query            
+            services.AddScoped<IPedidoQueryRepository, PedidoQueryRepository>();
+            
+        }
+    }
+}
