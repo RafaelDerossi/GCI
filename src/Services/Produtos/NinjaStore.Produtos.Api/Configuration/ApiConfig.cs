27049a92b6d723a96da259ec2e8aca129fb279f3
Rafael Derossi <rafael_derossi@hotmail.com>
2021-10-06 11:16:55 -0300

    Api de Produto

---
+++ src/Services/Produtos/NinjaStore.Produtos.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,72 @@
+﻿using NinjaStore.WebApi.Core.Identidade;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NinjaStore.Produtos.Infra.Data;
+
+namespace NinjaStore.Produtos.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        private const string PermissoesEspecificasDeOrigem = "_permissoesEspecificasDeOrigem";
+
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+
+            //Contexts
+            services.AddDbContext<ProdutoContextDB>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ProdutoConnection")));
+
+            //Query Contexts
+            services.AddDbContext<ProdutoQueryContextDB>(options =>
+              options.UseSqlServer(configuration.GetConnectionString("QueryConnection")));        
+
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy(PermissoesEspecificasDeOrigem,
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(PermissoesEspecificasDeOrigem);
+
+            app.UseAuthConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/Services/Produtos/NinjaStore.Produtos.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,34 @@
+﻿using NinjaStore.Core.Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using FluentValidation.Results;
+using NinjaStore.Produtos.Aplication.Commands;
+using NinjaStore.Produtos.Aplication.Events;
+using NinjaStore.Produtos.Domain.Interfaces;
+using NinjaStore.Produtos.Infra.Data.Repository;
+using NinjaStore.Produtos.Aplication.Query;
+
+namespace NinjaStore.Produtos.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {            
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+            //Produto
+            services.AddScoped<IRequestHandler<AdicionarProdutoCommand, ValidationResult>, ProdutoCommandHandler>();            
+            services.AddScoped<INotificationHandler<ProdutoAdicionadoEvent>, ProdutoEventHandler>();
+
+            //Query
+            services.AddScoped<IProdutoQuery, ProdutoQuery>();            
+            
+            //Repositório
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();            
+            
+            //Repositório Query
+            services.AddScoped<IProdutoQueryRepository, ProdutoQueryRepository>();
+            
+        }
+    }
+}
